5096c075427065c72a620e47b437e3b6054ca1c2
aaronmivo <aamivo93@gmail.com>
2022-01-19 15:51:55 -0800

    Added reusable-textfield

---
+++ src/components/TextField/TextField.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+interface ITextFieldProps {
+  customClassNames?: string;
+  value?: string;
+  label?: string;
+  type: string;
+  name?: string;
+  placeholder?: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+function TextField(props: ITextFieldProps) {
+  return (
+    <label>
+      {props.label}
+      <input
+        className={`${props.customClassNames}`}
+        type={props.type}
+        value={props.value}
+        placeholder={props.placeholder}
+        onChange={props.onChange}
+        name={props.name}
+      />
+    </label>
+  );
+}
+
+export default TextField;
