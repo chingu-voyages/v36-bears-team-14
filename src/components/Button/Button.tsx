c9612fba022561700de4e6c5a329ff807bde912b
David Eastmond <davebuck1981@gmail.com>
2022-01-23 18:20:39 -0500

    continued button config

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,19 @@
 import "./button-style.css";
+import WhitePlusButton from "./white-plus-button.svg";
+import GreenPlusButton from "./green-plus-button.svg";
 interface IButtonProps {
-  buttonText: string;
+  buttonText?: string;
   customClassNames?: string;
   customTextClassNames?: string;
   onClick?: () => void;
+  plusButton: {
+    yes: boolean;
+    color: "white" | "green";
+  };
 }
 
 function Button(props: IButtonProps) {
-  return (
+  return !props.plusButton.yes ? (
     <div
       className={`Button__main ${
         props.customClassNames ? props.customClassNames : ""
@@ -20,6 +26,19 @@ function Button(props: IButtonProps) {
       >
         {props.buttonText}
       </div>
+    </div>
+  ) : (
+    <div
+      className={`Button__main ${
+        props.customClassNames ? props.customClassNames : ""
+      }`}
+    >
+      <img
+        src={`${
+          props.plusButton.color === "white" ? WhitePlusButton : GreenPlusButton
+        }`}
+        alt="add"
+      />
     </div>
   );
 }
