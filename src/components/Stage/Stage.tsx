97ae8ec0d875971cd84308b614995772d2cb7f67
David Eastmond <davebuck1981@gmail.com>
2022-01-31 22:56:16 -0500

    added to scene

--- src/components/Stage/Stage.tsx
+++ src/components/Stage/Stage.tsx
@@ -6,6 +6,7 @@ import { EAppScene } from "../../services/app/app.types";
 import "./stage-style.css";
 // Here is where we load different screens, either modal or "full page under the nav bar"
 // We can use a global redux store to keep track of which scene is active at a given time
+// We may need to make this into a stack data structure but keep it simple for now
 
 function renderCurrentScene(scene: EAppScene) {
   switch (scene) {
@@ -13,6 +14,10 @@ function renderCurrentScene(scene: EAppScene) {
       return <LoginScene />;
     case EAppScene.Register:
       return <RegistrationScene />;
+    default:
+      throw new Error(
+        "Define your scene in the EAppScene enum and add it to the case"
+      );
   }
 }
 function Stage() {
